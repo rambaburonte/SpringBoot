1d22e6d41a93d8fad14f98fcb56eea9548feb14b
rambaburonte <147910929+rambaburonte@users.noreply.github.com>
2025-01-07 17:58:47 +0530

    creating project by TL

---
+++ BootMvcProj01Git_WishMesseageGen/src/main/java/com/example/demo/BootMvcProj01GitWishMesseageGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj01GitWishMesseageGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj01GitWishMesseageGenApplication.class, args);
+	}
+
+}
